1a2bf6c164bd2b33bd61d3d35bab0ba142675e8f
Andrew Webb <webba@ihopu.org>
2021-05-11 15:24:09 -0500

    created party form on DOM

--- src/scripts/PartyForm.js
+++ src/scripts/PartyForm.js
@@ -2,8 +2,36 @@
 
 export const PartyForm = () => {
     return `
-        <div></div>
-        <div></div>
+        <div class="field">
+            <label class="label"
+            for="partyParentName">Parent Name</label>
+            <input type="text" name="partyParentName" class="input" />
+        </div>
+
+        <div class="field">
+            <label class="label"
+            for="partyChildName">Child Name</label>
+            <input type="text" name="partyChildName" class="input" />
+        </div>
+
+        <div class="field">
+            <label class="label"
+            for="partyNumberOfChildren">Number Of Children Attending</label>
+            <input type="text" name="partyNumberOfChildren" class="input" />
+        </div>
+
+        <div class="field">
+            <label class="label"
+            for="partyAddress">Address</label>
+            <input type="text" name="partyAddress" class="input" />
+        </div>
+
+        <div class="field">
+            <label class="label"
+            for="partyDate">Party Date</label>
+            <input type="date" name="partyDate" class="input" />
+        </div>
         
+        <button class="button" id="submitRequest">Submit Request</button>
     `
 }
